1df66a0eb640dee073e1ddb493c624690c09784b
Pratap kute <pratap.kute97@gmail.com>
2022-08-17 16:13:38 +0530

    todolist and todoitem model register to admin

--- todo_list/todo_app/admin.py
+++ todo_list/todo_app/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from todo_list.todo_app.models import ToDoItem, TodoList
+
+admin.site.register(ToDoItem)
+admin.site.register(TodoList)
