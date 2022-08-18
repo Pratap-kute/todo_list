29f4ef82e5c16f048e14aceb45c59dc7df2445d0
Pratap kute <pratap.kute97@gmail.com>
2022-08-18 12:10:44 +0530

    class base view and urls for todolist and itemlist

--- todo_list/todo_app/urls.py
+++ todo_list/todo_app/urls.py
@@ -1,6 +1,11 @@
-# from django.urls import include, path
+from django.urls import path
+
+from todo_list.todo_app.views import list_ItemTodolists_view, list_todolists_view
 
 # always metion the app name in the urls file
 app_name = "todo_app"
 
-urlpatterns = []
+urlpatterns = [
+    path("", list_todolists_view, name="todolists_index"),
+    path("list/<int:list_id>/", list_ItemTodolists_view, name="list"),
+]
--- todo_list/todo_app/views.py
+++ todo_list/todo_app/views.py
@@ -1,3 +1,35 @@
-# from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from todo_list.todo_app.models import ToDoItem, TodoList
+
+
+class ListTodoLists(ListView):
+    model = TodoList
+    template_name = "todo_app/index.html"
+    # context_object_name = "object_list"
+    # paginate_by = 10
+    # ordering = ["title"]
+    # queryset = TodoList.objects.all()
+
+
+list_todolists_view = ListTodoLists.as_view()
+
+
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo_app/item_list.html"
+    # context_object_name = "object_list"
+    # paginate_by = 10
+    # ordering = ["-created_date"]
+    # queryset = ToDoItem.objects.all()
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = TodoList.objects.get(id=self.kwargs["list_id"])
+        return context
+
+
+list_ItemTodolists_view = ItemListView.as_view()
